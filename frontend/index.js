6f5aff634492d576e16f02e8945e1eb60163419a
Nyamador <dselasi4u@gmail.com>
2021-02-24 17:14:26 +0000

    React bundles

--- frontend/index.js
+++ frontend/index.js
@@ -1,14 +1,6 @@
-import React, {useState} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+import './styles/main.less'
+import App from './components/App'
 
-const Index = () => {
-  const [number, setnumber] = useState(1);
-  return (
-    <>
-  <div>Hello React and django! {number} increased</div>
-  <button onClick={() => setnumber(number + 1)}>Click me</button>
-  </>
-  );
-};
-
-ReactDOM.render(<Index />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
