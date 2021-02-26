59806597f890206681f65b5d64f8b104d2c0956a
Nyamador <dselasi4u@gmail.com>
2021-02-26 09:46:34 +0000

    Added OTP model

--- payment/models.py
+++ payment/models.py
@@ -9,6 +9,7 @@ class Transaction(models.Model):
     amount = models.DecimalField("Amount", decimal_places=2, max_digits=100, default=5.00)
     customer_name = models.CharField("Customer ID", max_length=250)
     customer_email = models.EmailField("Customer Email", max_length=250)
+    customer_mobile = models.CharField("Customer Mobile", max_length=10)
     description = models.CharField("Transaction Description", max_length=500)
     payment_method = models.CharField("Payment Method", max_length=10)
     date_created = models.DateField("Date Created", auto_now_add=True)
@@ -16,6 +17,14 @@ class Transaction(models.Model):
 
     def __str__(self):
         return self.id
+
+class Otp(models.Model):
+    reference = models.UUIDField(default=uuid.uuid4, editable=False)
+    expiry = models.PositiveSmallIntegerField("Expiry in Minutes", default=5)
+    date_created = models.DateField("Date Created", auto_now_add=True)
+
+    def __str__(self):
+        return self.reference
 
 
 class Refund(models.Model):
