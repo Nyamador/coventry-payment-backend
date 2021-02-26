ae8192956f1f5ebb852fc953ad21e65aff0d5eea
Nyamador <dselasi4u@gmail.com>
2021-02-26 09:55:24 +0000

    Added customer model

--- users/models.py
+++ users/models.py
@@ -18,3 +18,12 @@ class CoventryUser(AbstractUser):
     def __str__(self):
         return self.email
 
+
+class Customer(models.Model):
+    firstname = models.CharField("Firstname", max_length=100)
+    lastname = models.CharField("Lastname", max_length=150)
+    email = models.EmailField("Email Address")
+    mobile = models.CharField("Mobile Number", max_length=20)
+
+    def __str__(self):
+        return self.firstname + self.lastname
