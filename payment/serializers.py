268c9a7da3de8a1b1120c3f4a46060aa32033c38
Nyamador <dselasi4u@gmail.com>
2021-02-26 10:52:36 +0000

    Added Otp and Link Serializers

--- payment/serializers.py
+++ payment/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
-from .models import Transaction
+from .models import Transaction, Otp, Link
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = [public_id, external_id, amount]
+
+
+class OTPSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Otp
+        fields = [reference, value, expiry, date_created]
+
+
+class LinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Link
+        fields = [uuid, date_created]
