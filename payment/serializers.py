88fdfb8d31454f41b9e90f8af69eda0226f99e21
Nyamador <dselasi4u@gmail.com>
2021-02-27 09:18:36 +0000

    Fixed syntax error in serializers

--- payment/serializers.py
+++ payment/serializers.py
@@ -4,16 +4,16 @@ from .models import Transaction, Otp, Link
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
-        fields = [public_id, external_id, amount]
+        fields = ['public_id', 'external_id', 'amount']
 
 
 class OTPSerializer(serializers.ModelSerializer):
     class Meta:
         model = Otp
-        fields = [reference, value, expiry, date_created]
+        fields = ['reference', 'value', 'expiry', 'date_created']
 
 
 class LinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Link
-        fields = [uuid, date_created]
+        fields = ['uuid', 'date_created']
