381120363dd24c7ba9f56cb3b5b4cf81613bd013
Nyamador <dselasi4u@gmail.com>
2021-03-01 23:33:05 +0000

    Fixed syntax error

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from payment.serializers import OTPSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'pages/index.html')
+
+class CreateOTP(generics.CreateAPIView):
+    serializer_class = OTPSerializer
+
+def VerifyOTP():
+    pass
