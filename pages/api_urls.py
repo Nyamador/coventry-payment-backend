f909daa02a93e034d26dde2f111c67f49c8288e8
Nyamador <dselasi4u@gmail.com>
2021-03-04 17:44:55 +0000

    Added otp api endpoint

--- pages/api_urls.py
+++ pages/api_urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
-    
+    path('otp', views.CreateOTP, name='create_otp'),
+    path('otp/verify', views.VerifyOTP, name='verify_otp'),
 ]
