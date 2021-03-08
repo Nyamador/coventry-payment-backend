2b4114df55fbb7fb678302a4bd6b5ed53247af26
Nyamador <dselasi4u@gmail.com>
2021-03-08 20:29:09 +0000

    Fixed syntax error in url conf

--- pages/api_urls.py
+++ pages/api_urls.py
@@ -3,11 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('otp/new', views.CreateOTP, name='create_otp'),
+    path('otp', views.OTPListCreate.as_view(), name='create_otp'),
     # path('otp/verify', views.VerifyOTP, name='verify_otp'),
     #Links
-    path('paymentlink', views.PaymentLinkCreation, name='create_paymentlink'),
+    path('paymentlink', views.PaymentLinkCreation.as_view(), name='create_paymentlink'),
     # Transactions
-    path('transactions/', views.TransactionList.as_view(), name='list_transactions'),
+    path('transactions', views.TransactionList.as_view(), name='list_transactions'),
     path('transaction/new', views.TransactionCreation.as_view(), name='create_transaction'),
 ]
