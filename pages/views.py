b69e9ff54cf3cdeb00b2682fece60b290d362260
Nyamador <dselasi4u@gmail.com>
2021-03-09 06:25:06 +0000

    Added Otp verification to otplistcreateview

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
 
 from payment.models import Transaction, Otp
 from payment.serializers import OTPSerializer, TransactionSerializer, LinkSerializer
@@ -18,6 +19,14 @@ class TransactionCreation(generics.CreateAPIView):
 class PaymentLinkCreation(generics.CreateAPIView):
     serializer_class = LinkSerializer
 
+
+
 class OTPListCreate(generics.ListCreateAPIView):
     serializer_class = OTPSerializer
     queryset = Otp.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        otp_id = request.query_params.get('ref')
+        query = Otp.objects.get(reference=otp_id)
+        serializer = self.get_serializer(query)
+        return Response(serializer.data)
