dad2895bda22a4d2446cf51e3858fab79eed4b1d
Desmond Nyamador <dselasi4u@gmail.com>
2021-03-22 23:13:38 +0000

    Added oauth2 urls

--- payment_gateway/urls.py
+++ payment_gateway/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
